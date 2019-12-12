f2a3977a42b4123afa9258fdffd12bf484d3c873
Marcin Czech <maczech@gmail.com>
2019-12-12 10:45:15 +0100

    Add default in memory BrokerApi implementation
    InProcessBrokerApi is a default implementation of the BrokerApi interface.
    
    Change-Id: I6d422d4a4afa6f21a2a1e93e5cde3e670b0cb532

--- src/main/java/com/gerritforge/gerrit/eventbroker/InProcessBrokerApi.java
+++ src/main/java/com/gerritforge/gerrit/eventbroker/InProcessBrokerApi.java
@@ -22,26 +22,18 @@ import com.google.common.collect.MapMaker;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.google.common.flogger.FluentLogger;
-import com.google.gson.Gson;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
-import java.util.UUID;
 import java.util.function.Consumer;
-import org.junit.Ignore;
 
-@Ignore
 public class InProcessBrokerApi implements BrokerApi {
   private static final FluentLogger log = FluentLogger.forEnclosingClass();
-  private final UUID instanceId;
-  private final Gson gson;
   private final Map<String, EvictingQueue<EventMessage>> messagesQueueMap;
   private final Map<String, EventBus> eventBusMap;
   private final Set<TopicSubscriber> topicSubscribers;
 
-  public InProcessBrokerApi(UUID instanceId) {
-    this.instanceId = instanceId;
-    this.gson = new Gson();
+  public InProcessBrokerApi() {
     this.eventBusMap = new MapMaker().concurrencyLevel(1).makeMap();
     this.messagesQueueMap = new MapMaker().concurrencyLevel(1).makeMap();
     this.topicSubscribers = new HashSet<>();
@@ -72,7 +64,7 @@ public class InProcessBrokerApi implements BrokerApi {
     topicEventConsumers.register(eventConsumer);
     topicSubscribers.add(topicSubscriber(topic, eventConsumer));
 
-    EvictingQueue<EventMessage> messageQueue = EvictingQueue.create(10);
+    EvictingQueue<EventMessage> messageQueue = EvictingQueue.create(100);
     messagesQueueMap.put(topic, messageQueue);
     topicEventConsumers.register(new EventBusMessageRecorder(messageQueue));
   }
@@ -89,7 +81,9 @@ public class InProcessBrokerApi implements BrokerApi {
 
   @Override
   public void replayAllEvents(String topic) {
-    messagesQueueMap.get(topic).stream().forEach(eventMessage -> send(topic, eventMessage));
+    if (messagesQueueMap.containsKey(topic)) {
+      messagesQueueMap.get(topic).stream().forEach(eventMessage -> send(topic, eventMessage));
+    }
   }
 
   private class EventBusMessageRecorder {
